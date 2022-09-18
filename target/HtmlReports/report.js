ffdd6fb10a70345c6056b5d8443e9575ac6059a9
birajdarcm <birajdarcm@gmail.com>
2022-09-18 23:32:46 +0530

    adding remaining folders

---
+++ target/HtmlReports/report.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("GoogleSearch.feature");
+formatter.feature({
+  "line": 1,
+  "name": "feature to test google search functionality",
+  "description": "",
+  "id": "feature-to-test-google-search-functionality",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Validate google search is working",
+  "description": "",
+  "id": "feature-to-test-google-search-functionality;validate-google-search-is-working",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "browser is open",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user is on google search page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters a text in search box",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "hits enter",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is navigated to search results",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.uri("login.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "#Author"
+    },
+    {
+      "line": 2,
+      "value": "#Date"
+    }
+  ],
+  "line": 3,
+  "name": "feature to test login functionality",
+  "description": "",
+  "id": "feature-to-test-login-functionality",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "Check login is successful with valid credentials",
+  "description": "",
+  "id": "feature-to-test-login-functionality;check-login-is-successful-with-valid-credentials",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "user is on login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters username and password",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "clicks on login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is naviagated to the home page",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
